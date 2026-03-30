d3d8c758d568696ddb030920004cef8c657b5b73
Shubhangi Singh <shubhangisinghtanwar@gmail.com>
2026-03-30 16:33:15 -0400

    Restore Loan Eligibility Prediction App code
    Restored the Loan Eligibility Prediction App code and set page configuration.

--- app.py
+++ app.py
@@ -1,72 +1,7 @@
-# #Loan Eligibility Prediction App
-# import streamlit as st
-# import joblib
-# import pandas as pd
-
-# from src.preprocessing import clean_data, encode_data
-
-# #Load trained model
-# model = joblib.load("models/loan_model.pkl")
-
-# st.title(" Loan Eligibility Prediction App")
-
-# st.write("Fill the details below to check if a loan will be approved.")
-
-# #User Inputs
-# gender = st.selectbox("Gender", ["Male", "Female"])
-# married = st.selectbox("Married", ["Yes", "No"])
-# dependents = st.selectbox("Dependents", ["0", "1", "2", "3+"])
-# education = st.selectbox("Education", ["Graduate", "Not Graduate"])
-# self_employed = st.selectbox("Self Employed", ["Yes", "No"])
-# applicant_income = st.number_input("Applicant Income", min_value=0)
-# coapplicant_income = st.number_input("Coapplicant Income", min_value=0)
-# loan_amount = st.number_input("Loan Amount", min_value=0)
-# loan_term = st.selectbox("Loan Amount Term", [360, 180, 120, 60])
-# credit_history = st.selectbox("Credit History", [1.0, 0.0])
-# property_area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
-
-# #Predict button
-# if st.button("Predict"):
-
-#     input_data = {
-#         "Gender": gender,
-#         "Married": married,
-#         "Dependents": dependents,
-#         "Education": education,
-#         "Self_Employed": self_employed,
-#         "ApplicantIncome": applicant_income,
-#         "CoapplicantIncome": coapplicant_income,
-#         "LoanAmount": loan_amount,
-#         "Loan_Amount_Term": loan_term,
-#         "Credit_History": credit_history,
-#         "Property_Area": property_area,
-#         "Loan_Approved": "Y"  # dummy (needed for preprocessing)
-#     }
-
-#     input_df = pd.DataFrame([input_data])
-
-#     #Apply same preprocessing
-#     input_df = clean_data(input_df)
-#     input_df = encode_data(input_df)
-
-#     #Drop target column
-#     input_df = input_df.drop("Loan_Approved", axis=1)
-
-#     #Align columns with training data
-#     columns = joblib.load("models/columns.pkl")
-#     input_df = input_df.reindex(columns=columns, fill_value=0)
-
-#     prediction = model.predict(input_df)[0]
-
-#     if prediction == 1:
-#         st.success(" Loan Approved")
-#     else:
-#         st.error("Sorry:) || Loan Not Approved")
-
+#Loan Eligibility Prediction App
 import streamlit as st
 import joblib
 import pandas as pd
-
 from src.preprocessing import clean_data, encode_data
 
 st.set_page_config(page_title="Loan Eligibility Predictor", page_icon="🏦", layout="centered")
